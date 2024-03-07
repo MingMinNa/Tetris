bbe149b620baa3de040f9c3b07b22e76910373b2
MingMinNa <n139726845@gmail.com>
2024-03-07 08:37:40 +0800

    add replay function

--- src/Tetris.java
+++ src/Tetris.java
@@ -5,17 +5,15 @@ import gameState.*;
 public class Tetris {
     public static int frame_width = 800, frame_height = 800;
     public static void main(String[] args) throws Exception {
-
         JFrame frame = new JFrame("Tetris");
         frame.setSize(frame_width, frame_height);
         
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        new HomeScreen(frame);
-        // System.out.println("Hello");
-        new GameScreen(frame);
-        frame.revalidate();
-        frame.repaint();
+        while(true){
+            new HomeScreen(frame);
+            new GameScreen(frame);
+        } 
     }
 }
