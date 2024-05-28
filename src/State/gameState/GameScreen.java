78fbbca24c497c5eed3898dd27414f70ec388f98
MingMinNa <n139726845@gmail.com>
2024-05-28 18:55:42 +0800

    add ghost piece(named "ghost block" in the code)

--- src/State/gameState/GameScreen.java
+++ src/State/gameState/GameScreen.java
@@ -34,6 +34,8 @@ public class GameScreen {
             Random rnd = new Random();
             preview_blocks[i] = new GameBlock(rnd.nextInt(7), rnd.nextInt(7));
         }
+        for(int i = 0;i < 4;i ++)
+            ghost_block_cells[i] = null;
 
         background_panel = new GamePanel();
         frame.getContentPane().add(background_panel);
@@ -88,7 +90,7 @@ public class GameScreen {
                 
                 if(auto_fall_ticks >= GAME_STATE_AUTO_FALL_TICK[current_speedup_state]){
                     auto_fall_ticks = 0;
-                    background_panel.cellPositionUpdate(game_area_cells);
+                    background_panel.blockPositionUpdate(game_area_cells);
         
                     if(tryMove("down") == false){
                         if(current_cells[3].getY() <= 2){
@@ -98,7 +100,7 @@ public class GameScreen {
                         current_block = null;
                         
                         gameLineCheck(game_area_cells);
-                        background_panel.cellPositionUpdate(game_area_cells);
+                        background_panel.blockPositionUpdate(game_area_cells);
                     }
                     background_panel.revalidate();
                     frame.revalidate();
@@ -113,7 +115,9 @@ public class GameScreen {
                     if(current_block == null){
                         currentBlockUpdate();
                         background_panel.paintPreviewBlock(preview_blocks);
+
                     }
+                    ghostBlockUpdate();
                     // keyboard operation
                     String pressed_key = key_handler.getCurrentKey();
 
@@ -158,14 +162,15 @@ public class GameScreen {
                     }
 
                     // the next left or right movement will be seen as the first one (reset)
-                    if(pressed_key.equals("null")){left_right_move = 0;}
+                    boolean nothing_pressed = pressed_key.equals("null");
+                    if(nothing_pressed){left_right_move = 0;}
 
-                    if(movement_ticks >= 2 && !pressed_key.equals("null"))
+                    if(movement_ticks >= 2 && nothing_pressed == false)
                         movement_ticks = 0;
                         
 
                     background_panel.stateColorChange(current_speedup_state);
-                    background_panel.cellPositionUpdate(game_area_cells);
+                    background_panel.blockPositionUpdate(game_area_cells);
                     background_panel.revalidate();
                     frame.revalidate();
                     frame.repaint();
@@ -320,6 +325,116 @@ public class GameScreen {
             }
             return rotate;
         }
+        private void ghostBlockUpdate(){
+            JLabel [][][] cell_borders = background_panel.getCellBorder();
+            // [0]: vertical, [1]: horizontal
+            for(int i = 0; i < 4; i ++){
+                if(ghost_block_cells[i] == null){
+                    ghost_block_cells[i] = new Cell(current_cells[i]);
+                    continue;
+                }
+                int last_x = ghost_block_cells[i].getX(), last_y = ghost_block_cells[i].getY() - 3;
+                if(last_y >= 0){
+                    cell_borders[0][last_y][last_x].setIcon(GamePanel.cell_img.get("gray"));
+                    cell_borders[0][last_y][last_x + 1].setIcon(GamePanel.cell_img.get("gray"));
+                    cell_borders[1][last_y][last_x].setIcon(GamePanel.cell_img.get("gray"));
+                    cell_borders[1][last_y + 1][last_x].setIcon(GamePanel.cell_img.get("gray"));    
+                }
+                ghost_block_cells[i].setX(current_cells[i].getX());
+                ghost_block_cells[i].setY(current_cells[i].getY());
+            }
+            while(checkMove(ghost_block_cells, game_area_cells, "down")){
+                for(int i = 0; i < 4;i ++)
+                    ghost_block_cells[i].setY(ghost_block_cells[i].getY() + 1);
+            }
+            for(int i = 0; i < 4; i++){
+                int last_x = ghost_block_cells[i].getX(), last_y = ghost_block_cells[i].getY() - 3;
+                if(last_y < 0 ) continue;
+                cell_borders[0][last_y][last_x].setIcon(GamePanel.cell_img.get(current_cells[0].getColor()));
+                cell_borders[0][last_y][last_x + 1].setIcon(GamePanel.cell_img.get(current_cells[0].getColor()));
+                cell_borders[1][last_y][last_x].setIcon(GamePanel.cell_img.get(current_cells[0].getColor()));
+                cell_borders[1][last_y + 1][last_x].setIcon(GamePanel.cell_img.get(current_cells[0].getColor()));
+            }
+    
+        }
+        private void currentBlockUpdate(){
+            // the preview_blocks[0] will become the current_block we can operate
+            int block_type = preview_blocks[0].getBlockType();
+            String block_color = preview_blocks[0].getColor();
+    
+            // preview_blocks[1] -> preview_blocks[0], random_block -> preview_blocks[1]
+            Random rnd = new Random();
+            preview_blocks[0].setBlockType(preview_blocks[1].getBlockType());
+            preview_blocks[0].setColor(preview_blocks[1].getColor());
+            preview_blocks[1].setBlockType(rnd.nextInt(7));
+            preview_blocks[1].setColor(rnd.nextInt(7));
+    
+            // initialize falling x_axis
+            int init_x = rnd.nextInt(0, 7);
+    
+            /*
+            * block_type
+            * 0: I block
+            *  * * * * * *
+            *  * * * * * *
+            *  x o x x * *
+            * 1: O block
+            *  * * * * * *
+            *  o x * * * *
+            *  x x * * * *
+            * 2: S block
+            *  * * * * * *
+            *  * x x * * *
+            *  x o * * * *
+            * 3: Z block
+            *  * * * * * *
+            *  x x * * * *
+            *  * o x * * *
+            * 4: J block 
+            *  * * * * * *
+            *  x * * * * * 
+            *  x o x * * * 
+            * 5: L block
+            *  * * * * * *
+            *  * * x * * *
+            *  x o x * * *
+            * 6: T block
+            *  * * * * * *
+            *  * x * * * * 
+            *  x o x * * * 
+            */
+            int [][][] colored_pos = {
+                {{2, 0}, {2, 1}, {2, 2}, {2, 3}}, 
+                {{1, 0}, {1, 1}, {2, 0}, {2, 1}}, 
+                {{1, 1}, {1, 2}, {2, 0}, {2, 1}}, 
+                {{1, 0}, {1, 1}, {2, 1}, {2, 2}}, 
+                {{1, 0}, {2, 2}, {2, 0}, {2, 1}}, 
+                {{1, 2}, {2, 2}, {2, 0}, {2, 1}}, 
+                {{1, 1}, {2, 2}, {2, 0}, {2, 1}}};
+            for(int i = 0; i < 4;i ++){
+    
+                game_area_cells[colored_pos[block_type][i][0]][colored_pos[block_type][i][1] + init_x].setColor(block_color);
+                current_cells[i] = game_area_cells[colored_pos[block_type][i][0]][colored_pos[block_type][i][1] + init_x];
+            }
+            // sort the current_cells based on x and y. (to simplify the current_block movement code)
+            // for example, assume current_cells has been sorted, then 
+            // current_cells[0].getY() > current_cells[1].getY() or 
+            // if current_cells[0].getY() == current_cells[1].getY(), current_cells[0].getX() < current_cells[1].getX()
+            for(int i = 0; i < 3;i ++){
+                for(int j = 0;j < 3;j ++){
+                    // y: down to up, x: left to right
+                    if(current_cells[j].getY() < current_cells[j+1].getY() || (current_cells[j].getY() == current_cells[j+1].getY() && current_cells[j].getX() > current_cells[j+1].getX())){
+                        Cell temp_cell = current_cells[j];
+                        current_cells[j] = current_cells[j + 1];
+                        current_cells[j + 1] = temp_cell;
+                    }
+                }
+            }
+            int [] center_cell_index = {1, 2, 1, 0, 1, 1, 1};    
+            current_block = new GameBlock(current_cells[center_cell_index[block_type]].getX(), current_cells[center_cell_index[block_type]].getY(),block_type, block_color);
+            
+            return;
+        }
     }
 
     // ----------------------------------------
@@ -331,84 +446,6 @@ public class GameScreen {
             background_panel.stateDisplayUpdate(current_speedup_state);
         }
         return next_state;
-    }
-    private void currentBlockUpdate(){
-        // the preview_blocks[0] will become the current_block we can operate
-        int block_type = preview_blocks[0].getBlockType();
-        String block_color = preview_blocks[0].getColor();
-
-        // preview_blocks[1] -> preview_blocks[0], random_block -> preview_blocks[1]
-        Random rnd = new Random();
-        preview_blocks[0].setBlockType(preview_blocks[1].getBlockType());
-        preview_blocks[0].setColor(preview_blocks[1].getColor());
-        preview_blocks[1].setBlockType(rnd.nextInt(7));
-        preview_blocks[1].setColor(rnd.nextInt(7));
-
-        // initialize falling x_axis
-        int init_x = rnd.nextInt(0, 7);
-
-        /*
-        * block_type
-        * 0: I block
-        *  * * * * * *
-        *  * * * * * *
-        *  x o x x * *
-        * 1: O block
-        *  * * * * * *
-        *  o x * * * *
-        *  x x * * * *
-        * 2: S block
-        *  * * * * * *
-        *  * x x * * *
-        *  x o * * * *
-        * 3: Z block
-        *  * * * * * *
-        *  x x * * * *
-        *  * o x * * *
-        * 4: J block 
-        *  * * * * * *
-        *  x * * * * * 
-        *  x o x * * * 
-        * 5: L block
-        *  * * * * * *
-        *  * * x * * *
-        *  x o x * * *
-        * 6: T block
-        *  * * * * * *
-        *  * x * * * * 
-        *  x o x * * * 
-        */
-        int [][][] colored_pos = {
-            {{2, 0}, {2, 1}, {2, 2}, {2, 3}}, 
-            {{1, 0}, {1, 1}, {2, 0}, {2, 1}}, 
-            {{1, 1}, {1, 2}, {2, 0}, {2, 1}}, 
-            {{1, 0}, {1, 1}, {2, 1}, {2, 2}}, 
-            {{1, 0}, {2, 2}, {2, 0}, {2, 1}}, 
-            {{1, 2}, {2, 2}, {2, 0}, {2, 1}}, 
-            {{1, 1}, {2, 2}, {2, 0}, {2, 1}}};
-        for(int i = 0; i < 4;i ++){
-
-            this.game_area_cells[colored_pos[block_type][i][0]][colored_pos[block_type][i][1] + init_x].setColor(block_color);
-            current_cells[i] = game_area_cells[colored_pos[block_type][i][0]][colored_pos[block_type][i][1] + init_x];
-        }
-        // sort the current_cells based on x and y. (to simplify the current_block movement code)
-        // for example, assume current_cells has been sorted, then 
-        // current_cells[0].getY() > current_cells[1].getY() or 
-        // if current_cells[0].getY() == current_cells[1].getY(), current_cells[0].getX() < current_cells[1].getX()
-        for(int i = 0; i < 3;i ++){
-            for(int j = 0;j < 3;j ++){
-                // y: down to up, x: left to right
-                if(current_cells[j].getY() < current_cells[j+1].getY() || (current_cells[j].getY() == current_cells[j+1].getY() && current_cells[j].getX() > current_cells[j+1].getX())){
-                    Cell temp_cell = current_cells[j];
-                    current_cells[j] = current_cells[j + 1];
-                    current_cells[j + 1] = temp_cell;
-                }
-            }
-        }
-        int [] center_cell_index = {1, 2, 1, 0, 1, 1, 1};    
-        current_block = new GameBlock(current_cells[center_cell_index[block_type]].getX(), current_cells[center_cell_index[block_type]].getY(),block_type, block_color);
-        
-        return;
     }
     private void gameLineCheck(Cell [][] game_area_cells){
         int del_line = 0;
@@ -429,7 +466,7 @@ public class GameScreen {
             else{
                 for(int j = 0; j < game_area_cells[0].length; j++){
                     game_area_cells[i][j].setColor("black");
-                    background_panel.cellPositionUpdate(game_area_cells);
+                    background_panel.blockPositionUpdate(game_area_cells);
                     try {Thread.sleep(50);}
                     catch(InterruptedException e) {e.printStackTrace();}
                 }
@@ -466,6 +503,7 @@ public class GameScreen {
     private Cell [][] game_area_cells = new Cell[23][10];
     private GameBlock [] preview_blocks = new GameBlock[2];
     private GameBlock current_block = null;
+    private Cell [] ghost_block_cells = new Cell[4];
     private Cell [] current_cells =  new Cell[4];
 
     // Game_state_data
@@ -630,7 +668,8 @@ class GamePanel extends JPanel{
         }
         return;
     }
-    public void cellPositionUpdate(Cell [][] game_area_cells){
+    public void blockPositionUpdate(Cell [][] game_area_cells){
+
         for(int i = 0; i < GAME_AREA_Y_CNT; i++){
             for(int j = 0; j < GAME_AREA_X_CNT; j++){
                 if (game_area_cells[i + 3][j].getColor().equals("black")){
@@ -702,14 +741,15 @@ class GamePanel extends JPanel{
         }
         this.revalidate();
     }
-
+    public JLabel[][][] getCellBorder(){
+        return this.label_cell_border;
+    }
     public GameEND getGameEndPanel(){
         return this.game_end_panel;
     }
     public GameKeyHandler getHandler(){
         return this.handler;
     }
-    
     // --------------------------------------
     private JLabel labelMake(int center_x, int center_y, String words, int words_width, int words_height){
         return labelMake(center_x, center_y, words, words_width, words_height, 20);
@@ -729,18 +769,20 @@ class GamePanel extends JPanel{
         // vertical border line
         int x_init = X_START + Cell.BLOCK_WIDTH / 2, y_init = Y_START;
         for(int i = 1; i <= GAME_AREA_Y_CNT; i ++){
-            for(int j = 1; j < GAME_AREA_X_CNT; j++){
+            for(int j = 0; j <= GAME_AREA_X_CNT; j++){
                 JLabel cell_border = labelMake(x_init + Cell.BLOCK_WIDTH * j + 1, y_init + Cell.BLOCK_HEIGHT * i , "",  3, Cell.BLOCK_HEIGHT - 3);
                 cell_border.setIcon(cell_img.get("gray"));
+                label_cell_border[0][i-1][j] = cell_border;
                 this.add(cell_border);
             }
         }
         // horizontal border line
         x_init = X_START; y_init = Y_START +  + Cell.BLOCK_HEIGHT / 2;
-        for(int i = 1; i < GAME_AREA_Y_CNT; i ++){
+        for(int i = 0; i <= GAME_AREA_Y_CNT; i ++){
             for(int j = 1; j <= GAME_AREA_X_CNT; j++){
                 JLabel cell_border = labelMake(x_init + Cell.BLOCK_WIDTH * j, y_init + Cell.BLOCK_HEIGHT * i + 1 , "",  Cell.BLOCK_WIDTH - 3, 3);
                 cell_border.setIcon(cell_img.get("gray"));
+                label_cell_border[1][i][j-1] = cell_border; 
                 this.add(cell_border);
             }
         }
@@ -751,9 +793,9 @@ class GamePanel extends JPanel{
             for(int j = 0;j < x_cnt; j++){
                 if(i == 0 || i == y_cnt - 1 || j == 0 || j == x_cnt - 1){
                     
-                    JLabel borderLabel = labelMake(x_init + Cell.BLOCK_WIDTH * j , y_init + Cell.BLOCK_HEIGHT * i , "", Cell.BLOCK_WIDTH - 4, Cell.BLOCK_HEIGHT - 4);
-                    borderLabel.setIcon(cell_img.get("gray"));
-                    this.add(borderLabel);
+                    JLabel border_label = labelMake(x_init + Cell.BLOCK_WIDTH * j , y_init + Cell.BLOCK_HEIGHT * i , "", Cell.BLOCK_WIDTH - 4, Cell.BLOCK_HEIGHT - 4);
+                    border_label.setIcon(cell_img.get("gray"));
+                    this.add(border_label);
                 }
                 else if(preview_or_game == true){
                     label_cells[i - 1][j - 1] = labelMake(X_START + Cell.BLOCK_WIDTH * j + 1,Y_START + Cell.BLOCK_HEIGHT * i + 1,"", Cell.BLOCK_WIDTH - 4, Cell.BLOCK_HEIGHT - 4);
@@ -803,10 +845,11 @@ class GamePanel extends JPanel{
         // To have border line, just remove the comments below. 
         buildCellBorderLine(); 
     }
-    private static JLabel [][] label_score_display = new JLabel[5][15]; // int score_height_cnt = 5, score_witdh_cnt = 15;
-    private static JLabel [][] label_cells = new JLabel[GAME_AREA_Y_CNT][GAME_AREA_X_CNT];
-    private static JLabel [][] label_preview = new JLabel[PREVIEW_AREA_Y_CNT - 2][PREVIEW_AREA_X_CNT - 2];
+    private JLabel [][] label_score_display = new JLabel[5][15]; // int score_height_cnt = 5, score_witdh_cnt = 15;
+    private JLabel [][] label_cells = new JLabel[GAME_AREA_Y_CNT][GAME_AREA_X_CNT];
+    private JLabel [][] label_preview = new JLabel[PREVIEW_AREA_Y_CNT - 2][PREVIEW_AREA_X_CNT - 2];
     
+    private JLabel [][][] label_cell_border = new JLabel[2][GAME_AREA_Y_CNT + 1][GAME_AREA_X_CNT + 1];
     private GameEND game_end_panel = null;
     private GameKeyHandler handler = null;
     private JLabel state_display = null;
--- src/blocks/Cell.java
+++ src/blocks/Cell.java
@@ -8,12 +8,20 @@ public class Cell {
         block_y_index = index_y;
         this.color = color;
     }
+    public Cell(Cell copied_cell){
+        block_x_index = copied_cell.getX();
+        block_y_index = copied_cell.getY();
+        this.color = copied_cell.getColor();
+    }
 
     public String getColor(){ return this.color;}
     public int getX(){return block_x_index;}
     public int getY(){return block_y_index;}
 
+    public void setX(int next_x){if(next_x >= 0 && next_x <= 9) block_x_index = next_x;}
+    public void setY(int next_y){if(next_y >= 0 && next_y <= 22) block_y_index = next_y;}
     public void setColor(String color){this.color = color;}
+
     // ------------------------------------
     private int block_x_index; // 0 ~ 9 (10)
     private int block_y_index; // 0 ~ 22(23)
